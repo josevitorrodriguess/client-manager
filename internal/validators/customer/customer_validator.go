7daae2a090638dc066257d507f62ef3877919a18
Jose <josevitorrodrigues17@gmail.com>
2025-04-07 10:27:46 -0300

    feat: add validations for customer pj

--- internal/validators/customer/customer_validator.go
+++ internal/validators/customer/customer_validator.go
@@ -48,3 +48,39 @@ func (cPFr *CustomerPFRequest) IsValid() (bool, error) {
 
 	return true, nil
 }
+
+type CustomerPJRequest struct {
+	Type        string      `json:"type"`
+	Email       string      `json:"email"`
+	Phone       string      `json:"phone"`
+	Cnpj        string      `json:"cnpj"`
+	CompanyName string      `json:"company_name"`
+	AddressType string      `json:"address_type"`
+	Street      string      `json:"street"`
+	Number      string      `json:"number"`
+	Complement  pgtype.Text `json:"complement"`
+	State       string      `json:"state"`
+	City        string      `json:"city"`
+	Cep         string      `json:"cep"`
+}
+
+
+func (cPJr *CustomerPJRequest) IsValid() (bool, error) {
+	if !(utils.MinChars(cPJr.CompanyName, 5) && utils.MaxChars(cPJr.CompanyName, 100)) {
+		return false, fmt.Errorf("company name must have between 5 and 100 characters")
+	}
+	if !utils.Matches(cPJr.Email, utils.EmailRegex) {
+		return false, fmt.Errorf("invalid email")
+	}
+	if !utils.Matches(cPJr.Phone, utils.PhoneRegex) {
+		return false, fmt.Errorf("invalid phone")
+	}
+	if !utils.Matches(cPJr.Cep, utils.CEPRegex) {
+		return false, fmt.Errorf("invalid cep")
+	}
+	if !utils.Matches(cPJr.Cnpj, utils.CNPJRegex) {
+		return false, fmt.Errorf("invalid cnpj")
+	}
+
+	return true, nil
+}
