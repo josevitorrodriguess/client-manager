27ea020cac63ca5273ae7d415ab4b6a94fd2eaf3
Jose <josevitorrodrigues17@gmail.com>
2025-04-08 17:50:09 -0300

    feat: helper to get uuid from session

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,8 +1,12 @@
 package api
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"github.com/josevitorrodriguess/client-manager/internal/services"
 )
 
@@ -11,4 +15,18 @@ type Api struct {
 	UserService     services.UserService
 	CustomerService services.CustomerService
 	Sessions        *scs.SessionManager
+}
+
+func GetAuthenticatedUserID(ctx context.Context, session *scs.SessionManager) (uuid.UUID, error) {
+	val := session.GetString(ctx, "AuthenticatedUserId") // já faz type assertion p/ string
+	if val == "" {
+		return uuid.Nil, fmt.Errorf("AuthenticatedUserId not found in session")
+	}
+
+	id, err := uuid.Parse(val)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid UUID in session: %w", err)
+	}
+
+	return id, nil
 }
