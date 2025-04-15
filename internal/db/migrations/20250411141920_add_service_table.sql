c4006c33ba060bf28b0fb37995e9f4bb68f36931
Jose <josevitorrodrigues17@gmail.com>
2025-04-15 16:05:19 -0300

    updt db

--- internal/db/migrations/20250411141920_add_service_table.sql
+++ internal/db/migrations/20250411141920_add_service_table.sql
@@ -4,13 +4,13 @@ SELECT 'up SQL query';
 -- +goose StatementEnd
 CREATE TABLE services (
     id SERIAL PRIMARY KEY,
-    customer_id UUID REFERENCES customers(id),
+    customer_id UUID NOT NULL REFERENCES customers(id),
     type_product VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    total_value DECIMAL(10, 2) NOT NULL,
-    down_payment DECIMAL(10, 2) NOT NULL,
-    is_paid BOOLEAN DEFAULT FALSE,
-    is_finished BOOLEAN DEFAULT FALSE
+    total_value NUMERIC(10, 2) NOT NULL,
+    down_payment NUMERIC(10, 2) NOT NULL,
+    is_paid BOOLEAN NOT NULL DEFAULT FALSE,
+    is_finished BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- +goose Down
